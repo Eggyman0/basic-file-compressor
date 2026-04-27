8d2a908092a472fd1694cd98dbdc396233c5cf99
Edmund <3ggyman@gmail.com>
2026-04-27 12:33:41 -0700

    Added small terminal UI to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,18 +2,48 @@
 #include <iostream>
 
 using namespace std;
+void choose(int choice, Huffman huff);
 
 int main() {
     Huffman huff;
-    if (huff.encodeFile("input.txt", "compressed.bin"))
-        cout << "\nFile compressed successfully\n";
-    else
-        cerr << "Error in file compression occured\n";
+    char yesno;
+    int choice;
 
-    if (huff.decodeFile("compressed.bin", "output.txt"))
-        cout << "\nFile decoded successfully\n";
-    else
-        cerr << "Error in file decoding occured\n";
-        
+    cout << "Would you like to encode or decode a file? (Y/N)\n";
+    cin >> yesno;
+    if (yesno == 'Y' || yesno == 'y') {
+        cout << "1) Encode File\n2) Decode File\n";
+        cin >> choice;
+        choose(choice, huff);
+    }
+    
     return 0;
+}
+
+void choose (int choice, Huffman huff) {
+    string file;
+    switch (choice) {
+    case 1:
+        cout << "Enter input text file (.txt)\n";
+        cin >> file;
+        if (huff.encodeFile(file, "compressed.bin")) 
+            cout << "\nFile compressed successfully\n";
+        else
+            cerr << "Error in file compression occured\n";
+        break;
+
+    case 2:
+        cout << "Enter name of compressed file:\n";
+        cin >> file;
+        if (huff.decodeFile(file, "output.txt"))
+            cout << "\nFile decoded successfully\n";
+        else
+            cerr << "Error in file decoding occured\n";
+        
+        break;
+
+    default:
+        
+        break;
+    }
 }
